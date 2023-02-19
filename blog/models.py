97f337f1e2eefa9c15821f21b237d3f5e53868a1
Uzytkownik <uzytkownik@kursGIT.pl>
2023-02-19 13:58:39 +0100

    Adding Meta class

--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,24 @@ class Category(models.Model):
     """ Describing categories of articles on blog"""
     name = models.CharField(max_length=30, unique=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
 
 class Article(models.Model):
     """ Describing articles on blog"""
-    title = models.CharField(max_length=30, unique=True)
+    title = models.CharField(max_length=250, unique=True)
     category = models.ForeignKey('blog.Category', on_delete=models.CASCADE)
     content = models.TextField()
     creation_datetime = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.title}'
 
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
