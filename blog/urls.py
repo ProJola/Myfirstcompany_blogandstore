8d3d48f64c382db9229c3d6a8f4b5c063f136ddb
Uzytkownik <uzytkownik@kursGIT.pl>
2023-02-19 15:16:06 +0100

    adding class-based view for category list display

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('test/', views.say_hello),
+    path('categories/', views.CategoriesListView.as_view(), name='categories_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, HttpResponse
+from django.urls import reverse
+from django.views.generic import CreateView, ListView
+
+from blog.models import Category
 
 
 def say_hello(request):
     return render(request, 'blog/test.html')
+
+
+class CategoriesListView(ListView):
+    model = Category
+    fields = '__all__'
+    template_name = 'blog/category_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+
+
 
