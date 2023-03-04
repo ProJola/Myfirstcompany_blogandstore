b48b7dc5c141e8083f88fd6e4210867fa58c6ca5
Uzytkownik <uzytkownik@kursGIT.pl>
2023-03-04 12:35:22 +0100

    adding detail of category view

--- blog/urls.py
+++ blog/urls.py
@@ -8,9 +8,8 @@ from .views import CategoriesDetailView
 urlpatterns = [
     path('test/', views.say_hello),
     path('categories/', views.CategoriesListView.as_view(), name='categories_list'),
-    path('categories/<int:pk>/', CategoriesDetailView.as_view(pattern_name='articles_list'), name='redirect_to_category'),
+    path('categories/<int:pk>/', CategoriesDetailView.as_view(), name='categories_detail'),
     path('articles/', views.ArticlesListView.as_view(), name='articles_list'),
-    path('articles/<int:pk>/', views.ArticlesDetailView.as_view(), name='redirect_to_article'),
     path('red/', RedirectView.as_view(url='https://www.youtube.com/'), name='go_to_youtube'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.urls import reverse
 from django.views.generic import CreateView, ListView, RedirectView, \
-    TemplateView
+    TemplateView, DetailView
 
 from blog.models import Category, Article
 
@@ -20,32 +20,25 @@ class CategoriesListView(ListView):
         return context
 
 
+class CategoriesDetailView(DetailView):
+    template_name = 'blog/categories_detail.html'
+    model = Category
+    fields = '__all__'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
 class ArticlesListView(ListView):
+    template_name = 'blog/categories_detail.html'
     model = Article
     fields = '__all__'
-    template_name = 'blog/article_list.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
 
-
-class CategoriesDetailView(RedirectView):
-    pattern_name = 'articles_list'
-
-    def get_redirect_url(self, *args, **kwargs):
-        detail = get_object_or_404(Category, pk=kwargs['pk'])
-        detail.update_counter()
-        return super().get_redirect_url(*args, **kwargs)
-
-
-class ArticlesDetailView(TemplateView):
-    template_name = 'article_list.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['articles'] = get_object_or_404(Category, pk=self.kwargs.get('pk'))
-        return context
 
 
 
